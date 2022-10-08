dd3b542ef62cc27ff526390bf54420bd7983e0b9
Kiran Tirunagiri <me190003064@iiti.ac.in>
2022-10-08 16:55:06 +0530

    Basic logic to block ads using DOM manipulation

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,1 +1,21 @@
-// TODO: content script
+const rules: {
+    [url: string]: () => void;
+} = {
+    "https://economictimes.indiatimes.com/": economictTimes,
+};
+
+function economictTimes() {
+    const parentOne = document.getElementById(
+        "google_ads_iframe_/7176/Economictimes/ET_Home/ET_Home_Home/ET_HP_Skin_RHS_125_0__container__"
+    );
+    console.log(parentOne);
+    // console.log(" i am present here bros");
+    const wrapper = document.getElementById("SkinningContainer");
+    parentOne.removeChild(wrapper);
+}
+
+if (document.URL in rules) {
+    rules[document.URL]();
+}
+
+// console.log("I am present here before the window addeventListener");
